4cf3395cf0bc3947024854cf4b9b22aaf65da6c2
omopekun michael <omopekunmichael@gmail.com>
2025-05-01 08:40:30 +0100

    fix: Configure Swagger UI endpoint and route prefix

--- Program.cs
+++ Program.cs
@@ -263,7 +263,11 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Social Media API v1");
+        c.RoutePrefix = "swagger";
+    });
 }
 else
 {
